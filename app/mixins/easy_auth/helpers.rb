4ee6c3db474c936e95dc30c930953381f554014b
Brian Cardarella <bcardarella@gmail.com>
2012-04-19 20:23:02 -0400

    Starting to normalize terms
    Using 'account' instead of 'user'

--- app/mixins/easy_auth/helpers.rb
+++ app/mixins/easy_auth/helpers.rb
@@ -2,25 +2,28 @@ module EasyAuth
   module Helpers
     def self.included(base)
       base.class_eval do
-        helper_method :current_user, :user_signed_in?, :user_signed_out?
+        helper_method :current_account, :current_user, :account_signed_in?, :user_signed_in?, :account_not_signed_in?, :user_not_signed_in?
       end
     end
 
-    def current_user
+    def current_account
       if session[:identity_id]
-        @current_user ||= EasyAuth::Identity.find(session[:identity_id]).account
+        @current_account ||= EasyAuth::Identity.find(session[:identity_id]).account
       end
 
-      @current_user
+      @current_account
     end
+    alias :current_user :current_account
 
-    def user_signed_in?
-      !!current_user
+    def account_signed_in?
+      current_account
     end
+    alias :user_signed_in? :account_signed_in?
 
-    def user_signed_out?
-      !user_signed_in?
+    def account_not_signed_in?
+      !account_signed_in?
     end
+    alias :user_not_signed_in? :account_not_signed_in?
 
   end
 end
--- spec/dummy/app/controllers/authenticated_controller.rb
+++ spec/dummy/app/controllers/authenticated_controller.rb
@@ -4,7 +4,7 @@ class AuthenticatedController < ApplicationController
   private
 
   def redirect_to_root_if_not_signed_in
-    if user_signed_out?
+    if user_not_signed_in?
       redirect_to root_path
     end
   end
