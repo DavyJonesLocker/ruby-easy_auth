136a2d5cf6a9abe36d98770ead3e0598f84bb9d8
Brian Cardarella <bcardarella@gmail.com>
2012-05-07 01:55:27 -0400

    Extracted route helpers into their own helpers

--- lib/easy_auth/routes.rb
+++ lib/easy_auth/routes.rb
@@ -1,16 +1,21 @@
 module ActionDispatch::Routing
   class Mapper
     def easy_auth_routes
-      # Password Reset
+      easy_auth_session_routes
+      easy_auth_password_reset_routes
+    end
+
+    def easy_auth_session_routes
+      get  '/sign_out' => 'sessions#destroy', :as => :sign_out
+      get  '/sign_in'  => 'sessions#new',     :as => :sign_in
+      post '/sign_in'  => 'sessions#create'
+    end
+
+    def easy_auth_password_reset_routes
       get  '/password_reset' => 'password_reset#new', :as => :password_reset
       post '/password_reset' => 'password_reset#create'
       get  '/password_reset/:reset_token' => 'password_reset#edit', :as => :edit_password
       put  '/password_reset/:reset_token' => 'password_reset#update'
-
-      # Authentication
-      get  '/sign_out' => 'sessions#destroy', :as => :sign_out
-      get  '/sign_in'  => 'sessions#new',     :as => :sign_in
-      post '/sign_in'  => 'sessions#create'
     end
   end
 end
