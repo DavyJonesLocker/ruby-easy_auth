350a189fa8505df251b8c207eab6d3156a82e2fb
Brian Cardarella <bcardarella@gmail.com>
2012-08-12 21:32:34 -0400

    Fixed for Travis

--- easy_auth.gemspec
+++ easy_auth.gemspec
@@ -26,7 +26,4 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'factory_girl_rails'
   s.add_development_dependency 'bourne'
   s.add_development_dependency 'launchy'
-  if RUBY_VERSION > '1.9' && RUBY_ENGINE == 'ruby'
-    s.add_development_dependency 'debugger'
-  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,28 +1,28 @@
-# Configure Rails Envinronment
+require 'rubygems'
+begin
+  require 'debugger'
+rescue LoadError
+end
+require 'bundler/setup'
+
 ENV['RAILS_ENV'] = 'test'
 require File.expand_path('../dummy/config/environment.rb',  __FILE__)
-
-require 'rspec/rails'
 require 'capybara/rspec'
+require 'rspec/rails'
 require 'capybara/email/rspec'
 require 'valid_attribute'
 require 'factory_girl_rails'
-if RUBY_VERSION > '1.9' && RUBY_ENGINE == 'ruby'
-  require 'debugger'
-end
 require 'bourne'
 
 ENGINE_RAILS_ROOT=File.join(File.dirname(__FILE__), '../')
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
 Dir[File.join(ENGINE_RAILS_ROOT, 'spec/support/**/*.rb')].each { |f| require f }
 Dir[File.join(ENGINE_RAILS_ROOT, 'spec/config/**/*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
   config.mock_with :mocha
   config.use_transactional_fixtures = true
-  config.include Factory::Syntax::Methods
+  config.include FactoryGirl::Syntax::Methods
 
   config.before(:each, :type => :request) do
     Dir[File.join(ENGINE_RAILS_ROOT, 'spec/requests/step_helpers/**/*.rb')].each { |f| require f }
