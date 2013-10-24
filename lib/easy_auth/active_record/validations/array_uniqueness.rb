7f0ac982f58deb488db1bd55c5588a49d80b85f8
Brian Cardarella <bcardarella@gmail.com>
2013-10-24 13:06:53 -0400

    Removed comments

--- lib/easy_auth/active_record/validations/array_uniqueness.rb
+++ lib/easy_auth/active_record/validations/array_uniqueness.rb
@@ -11,13 +11,6 @@ module ActiveRecord::Validations
       value  = value.to_s[0, column.limit] if value && column.limit && column.text?
 
       table[attribute].overlap(value)
-      # if !options[:case_sensitive] && value && column.text?
-        # # will use SQL LOWER function before comparison, unless it detects a case insensitive collation
-        # klass.connection.case_insensitive_comparison(table, attribute, column, value)
-      # else
-        # value = klass.connection.case_sensitive_modifier(value) unless value.nil?
-        # table[attribute].eq(value)
-      # end
     end
   end
 end
