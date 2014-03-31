fbad565078d40e9a70a85e5c652ce8ebac6bd910
Brian Cardarella <bcardarella@gmail.com>
2014-03-31 15:43:18 -0400

    Move to helper

--- lib/easy_auth/controllers/authenticated.rb
+++ lib/easy_auth/controllers/authenticated.rb
@@ -4,16 +4,4 @@ module EasyAuth::Controllers::Authenticated
   included do
     before_filter :attempt_to_authenticate
   end
-
-  private
-
-  def attempt_to_authenticate
-    if account_not_signed_in?
-      session[:requested_path] = request.method == 'GET' ? request.fullpath :  request.referer
-      respond_to do |format|
-        format.html { redirect_to main_app.sign_in_url }
-        format.json { render :json => {}, :status => 401 }
-      end
-    end
-  end
 end
